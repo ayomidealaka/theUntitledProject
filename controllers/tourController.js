64aeb10ae256ab2eddb5e1ac4ee7bd960e40fc14
ayomideAlaka <muizzyomide@gmail.com>
2020-09-06 02:24:13 +0100

    certain prettier formats commits.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,14 +4,24 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+//Checks for name or price when created.
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price',
+    });
+  }
+  next();
+};
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
     results: tours.length,
     data: {
-      tours
-    }
+      tours,
+    },
   });
 };
 
@@ -19,22 +29,23 @@ exports.getTour = (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1; //converts req.params.id to number
 
-  const tour = tours.find(el => el.id === id);
+  const tour = tours.find((el) => el.id === id);
   //Checking if tour is undefined.
   if (!tour) {
     return res.status(404).json({
       status: 'fail',
-      message: 'Invlaid ID'
+      message: 'Invlaid ID',
     });
   }
 
   res.status(200).json({
     status: 'success',
     data: {
-      tours: tour
-    }
+      tours: tour,
+    },
   });
 };
+
 exports.createTour = (req, res) => {};
 
 exports.updateTour = (req, res) => {};
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,15 +2,15 @@ const express = require('express');
 const tourController = require('./../controllers/tourController');
 const router = express.Router();
 
-router.param("id", (req, res, next, val)=>{
+router.param('id', (req, res, next, val) => {
   console.log(`Id is ${val}`);
   next();
-})
+});
 
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.checkBody, tourController.createTour);
 
 router
   .route('/:id')
