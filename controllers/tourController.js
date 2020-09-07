26081c3d86ce5b6f4c578555ac54e4acdc4542d7
ayomideAlaka <muizzyomide@gmail.com>
2020-09-07 01:31:12 +0100

    Refactored all functions to meet model standard.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,4 @@
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+const Tour = require('./../models/tourModel');
 
 //Checks for name or price when created.
 exports.checkBody = (req, res, next) => {
@@ -14,40 +10,108 @@ exports.checkBody = (req, res, next) => {
   }
   next();
 };
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
-};
 
-exports.getTour = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1; //converts req.params.id to number
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
 
-  const tour = tours.find((el) => el.id === id);
-  //Checking if tour is undefined.
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invlaid ID',
+    res.status(200).json({
+      status: 'success',
+      requestedAt: req.requestTime,
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'success',
+      error: err,
     });
   }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tours: tour,
-    },
-  });
 };
 
-exports.createTour = (req, res) => {};
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
 
-exports.updateTour = (req, res) => {};
+    // console.log(req.params);
+    // const id = req.params.id * 1; //converts req.params.id to number
 
-exports.deleteTour = (req, res) => {};
+    // const tour = tours.find((el) => el.id === id);
+    // //Checking if tour is undefined.
+    // if (!tour) {
+    //   return res.status(404).json({
+    //     status: 'fail',
+    //     message: 'Invlaid ID',
+    //   });
+    // }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tours: tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tours: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
