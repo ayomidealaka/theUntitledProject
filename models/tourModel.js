a82217837eca2bffd5e09169c4f8a97243120fb5
ayomideAlaka <muizzyomide@gmail.com>
2020-09-07 05:59:55 +0100

    Added more fields to DB.

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,15 +6,51 @@ const newSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'Must have duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Must have group size'],
+  },
+  difficulty: {
+    type: Number,
+    required: [true, 'Must have difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'Must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: [true, 'Must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'Must have n imageCover price'],
+  },
+  images: [String], //Array Of Strings.
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', newSchema);
