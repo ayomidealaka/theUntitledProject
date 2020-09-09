c9dd4b6bba2e2da005ecbf285995939a8fd3ea9d
ayomideAlaka <muizzyomide@gmail.com>
2020-09-09 20:58:12 +0100

    For basic API features.

---
+++ utils/apiFeatures.js
@@ -0,0 +1,61 @@
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
+
+    //Exclude fields from search query
+    const fieldsExcluded = ['page', 'sort', 'limit', 'fields'];
+    fieldsExcluded.forEach((el) => delete queryObj[el]);
+
+    //compose query.
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(
+      /\b(gte|get|lte|lt)\b/g,
+      (match) => `$${match}`
+    );
+    console.log(JSON.parse(queryStr));
+
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      console.log(sortBy);
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-createdAt');
+    }
+
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryStrings.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+
+    return this;
+  }
+
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(2).limit(limit);
+
+    return this;
+  }
+}
+
+module.exports = APIFeatures;
