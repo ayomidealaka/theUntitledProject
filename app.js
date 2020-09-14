29ea7cb53218310fb9de6f22650d8e4e981337fd
ayomideAlaka <muizzyomide@gmail.com>
2020-09-14 11:40:11 +0100

    Added middleware to handle unhandled routes.

--- app.js
+++ app.js
@@ -11,12 +11,6 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-//Global Middleware.
-app.use((req, res, next) => {
-  console.log('Hello from the middleware!!!');
-  next();
-});
-
 app.use((req, res, next) => {
   //Prints date and time.
   req.requestTime = new Date().toISOString();
@@ -25,5 +19,13 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+//Middleware to handle unhandled routes.
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `cannot find ${req.url} on this server`,
+  });
+});
 
 module.exports = app;
