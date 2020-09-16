fc6b6c5d36be99af20017c19c25aa7c59658aac6
ayomideAlaka <muizzyomide@gmail.com>
2020-09-16 23:09:40 +0100

    corrected error output bugs.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,10 +2,11 @@ const Tour = require('./../models/tourModel');
 const { query } = require('express');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
-  req.query.sort = '-ratingAverage.price';
+  req.query.sort = '-ratingsAverage.price';
   req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
   next();
 };
@@ -43,10 +44,14 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
 
+  if (!tour) {
+    return next(new AppError('No tour found for this ID', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
-      tours: tour,
+      tour,
     },
   });
 });
@@ -57,7 +62,7 @@ exports.createTour = catchAsync(async (req, res, next) => {
   res.status(201).json({
     status: 'success',
     data: {
-      tours: newTour,
+      tour: newTour,
     },
   });
 });
@@ -68,16 +73,24 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     runValidators: true,
   });
 
+  if (!tour) {
+    return next(new AppError('No tour found for this ID', 404));
+  }
+
   res.status(201).json({
     status: 'success',
     data: {
-      tours,
+      tour,
     },
   });
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('No tour found for this ID', 404));
+  }
 
   res.status(204).json({
     status: 'success',
