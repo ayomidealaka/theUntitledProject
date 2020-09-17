170fccdd492473a29e65a674b4dba2c15756156e
ayomideAlaka <muizzyomide@gmail.com>
2020-09-18 00:06:53 +0100

    Created MongoDB user model.

---
+++ models/userModel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+//name, email, photo, password
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm password'],
+    validate: {
+      //works on only create and save.
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same',
+    },
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  //only run fn if password is modified
+  if (!this.isModified('password')) return next();
+
+  //Hash passsword with cost of 12.
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //delete passwordConfirm field
+  this.passwordConfirm = undefined;
+
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
