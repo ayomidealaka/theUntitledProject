594e4ab73155d1ee7b84817e8ad5ec94b7f41f5a
ayomideAlaka <muizzyomide@gmail.com>
2020-09-23 18:33:18 +0100

    Added req.headers to check header.

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
   //Prints date and time.
   req.requestTime = new Date().toISOString();
+  // console.log(req.headers);
   next();
 });
 
