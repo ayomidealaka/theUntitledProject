c91751c44011f26aa28e75581259420d444efd05
ayomideAlaka <muizzyomide@gmail.com>
2020-09-23 18:34:16 +0100

    Added login, signup and JWT.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,14 @@
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const signToken = (id) => {
+  return jwt.sign({ id: id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -10,9 +18,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = signToken(newUser._id);
 
   //Send response
   res.status(201).json({
@@ -22,4 +28,67 @@ exports.signup = catchAsync(async (req, res, next) => {
       user: newUser,
     },
   });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //check if email and password exists.
+  if (!email || !password) {
+    next(new AppError('Please provide email and password', 400));
+  }
+
+  //check if user exists and password is correct
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  //If everything ok, send token to client.
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+});
+
+//middleware before accessing user routes
+exports.protect = catchAsync(async (req, res, next) => {
+  //Check if Token exists.
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access.', 401)
+    );
+  }
+
+  //Verify token.
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  //Check if user still exists.
+  const CurrentUser = User.findById(decoded.id);
+  if (!CurrentUser) {
+    return next(
+      new AppError('User belonging to this token does no longer exist.', 401)
+    );
+  }
+
+  //Check if user changed password.
+  if (CurrentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('user recently chabged password! Please login again.', 401)
+    );
+  }
+
+  req.user = CurrentUser;
+  next();
 });
