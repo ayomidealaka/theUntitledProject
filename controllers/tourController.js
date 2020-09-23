f70d249c7f4d2b81c2a469a86f9cd3cee7895289
ayomideAlaka <muizzyomide@gmail.com>
2020-09-23 18:36:01 +0100

    Removed very useless import.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,4 @@
 const Tour = require('./../models/tourModel');
-const { query } = require('express');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
